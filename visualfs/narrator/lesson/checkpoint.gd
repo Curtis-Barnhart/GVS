ae32b9c0a5f9e629bf38d7252c2812c17bfdf1cd
Curtis Barnhart <cbarnhart@westmont.edu>
2025-02-14 09:30:59 -0800

    Made initial scene layout for popup menu taking text input from user

--- visualfs/narrator/lesson/checkpoint.gd
+++ visualfs/narrator/lesson/checkpoint.gd
@@ -1,1 +1,11 @@
-extends Node
+extends RefCounted
+
+const FSManager = GVSClassLoader.gvm.filesystem.Manager
+
+var _fs_man: FSManager
+
+
+func _init(
+    fs_manager: FSManager
+) -> void:
+    self._fs_man = fs_manager
