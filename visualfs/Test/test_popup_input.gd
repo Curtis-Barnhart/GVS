204888583c98a7d65c10cd43bcf59efa24a7bcae
Curtis Barnhart <cbarnhart@westmont.edu>
2025-02-15 00:09:54 -0800

    Created popup text input scene

---
+++ visualfs/Test/test_popup_input.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+const PopupInput = GVSClassLoader.visualfs.narrator.PopupInput
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+func _draw() -> void:
+    self.draw_rect(Rect2(0, 0, 738, 160), Color.RED, false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+
+func _on_button_pressed() -> void:
+    print("Button pressed!")
+
+
+func _on_button_2_pressed() -> void:
+    print("Popup made")
+    var p: PopupInput = PopupInput.make_new()
+    p.user_input.connect(func (msg: String): print("User message: " + msg))
+    p.user_cancelled.connect(func (): print("User cancelled popup"))
+    p.popup(self)
+
+
+func _on_button_3_pressed() -> void:
+    print("Other button pressed")
--- visualfs/narrator/popup_input.gd
+++ visualfs/narrator/popup_input.gd
@@ -19,11 +19,14 @@ signal user_cancelled
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     self._border.size = self._container.size
-    pass # Replace with function body.
 
 
 static func make_new() -> PopupInput:
     return PopupInputScene.instantiate()
+
+
+func popup(tree_access: Node) -> void:
+    tree_access.get_tree().get_root().add_child(self)
 
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
@@ -35,9 +38,6 @@ func _on_cancel_pressed() -> void:
     self.queue_free()
 
 
-func _on_child_focus_exited() -> void:
-    if self.get_viewport().gui_get_focus_owner() not in [
-        self._container, self._ledit, self._cancel_button
-    ]:
-        self.user_cancelled.emit()
-        self.queue_free()
+func _gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        self._on_cancel_pressed()
