c32293bbaaf7ba665ca9ad869c1f2e1f522e0442
Curtis Barnhart <cbarnhart@westmont.edu>
2025-02-27 09:21:44 -0800

    Circular menu now does not trigger its own popup

--- visual/buttons/circle_menu.gd
+++ visual/buttons/circle_menu.gd
@@ -1,21 +1,15 @@
-extends Control
+extends Node2D
 
 var _children: Array
-var _menu_open: bool = false
 var _rad_in: float = 60
 var _rad_out: float = 150
 var _selected: int = -1
 
-signal menu_opened
 signal menu_closed(selection: int)
 
 
-func _center() -> Vector2:
-    return self.size / 2
-
-
-func _global_center() -> Vector2:
-    return self.global_position + self._center()
+func popup(tree_access: Node) -> void:
+    tree_access.get_tree().get_root().add_child(self)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,88 +19,69 @@ func _ready() -> void:
     
     var segment_width: float = 2*PI / len(self._children)
     for i in range(len(self._children)):
-        self._children[i].position = Vector2((self._rad_out - self._rad_in)/2 + self._rad_in, 0).rotated(segment_width * (i + 0.5)) + self._center()
+        self._children[i].position = Vector2((self._rad_out - self._rad_in)/2 + self._rad_in, 0).rotated(segment_width * (i + 0.5)) + Vector2.ZERO
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     pass
 
 
 func _draw() -> void:
-    if self._menu_open:
-        draw_circle(self._center(), self._rad_in, Color.STEEL_BLUE, false, 6)
-        draw_circle(self._center(), self._rad_out, Color.STEEL_BLUE, false, 6)
-        var segment_width: float = 2*PI / len(self._children)
-        for child_index in range(len(self._children)):
-            var line_start: Vector2 = Vector2(self._rad_in, 0).rotated(segment_width * child_index)
-            draw_line(
-                self._center() + line_start,
-                self._center() + self._rad_out/self._rad_in*line_start,
-                Color.STEEL_BLUE,
-                6
-            )
-        
-        if self._selected != -1:
-            draw_arc(
-                self._center(),
-                self._rad_in,
-                segment_width * self._selected,
-                segment_width * (1 + self._selected),
-                64,
-                Color.FIREBRICK,
-                8
-            )
-            draw_arc(
-                self._center(),
-                self._rad_out,
-                segment_width * self._selected,
-                segment_width * (1 + self._selected),
-                64,
-                Color.FIREBRICK,
-                8
-            )
-            var line_start: Vector2 = Vector2(self._rad_in, 0).rotated(segment_width * self._selected)
-            draw_line(self._center() + line_start, self._center() + self._rad_out/self._rad_in*line_start, Color.FIREBRICK, 8)
-            line_start = Vector2(self._rad_in, 0).rotated(segment_width * (1 + self._selected))
-            draw_line(self._center() + line_start, self._center() + self._rad_out/self._rad_in*line_start, Color.FIREBRICK, 8)
+    draw_circle(Vector2.ZERO, self._rad_in, Color.STEEL_BLUE, false, 6)
+    draw_circle(Vector2.ZERO, self._rad_out, Color.STEEL_BLUE, false, 6)
+    var segment_width: float = 2*PI / len(self._children)
+    for child_index in range(len(self._children)):
+        var line_start: Vector2 = Vector2(self._rad_in, 0).rotated(segment_width * child_index)
+        draw_line(
+            Vector2.ZERO + line_start,
+            Vector2.ZERO + self._rad_out/self._rad_in*line_start,
+            Color.STEEL_BLUE,
+            6
+        )
+    
+    if self._selected != -1:
+        draw_arc(
+            Vector2.ZERO,
+            self._rad_in,
+            segment_width * self._selected,
+            segment_width * (1 + self._selected),
+            64,
+            Color.FIREBRICK,
+            8
+        )
+        draw_arc(
+            Vector2.ZERO,
+            self._rad_out,
+            segment_width * self._selected,
+            segment_width * (1 + self._selected),
+            64,
+            Color.FIREBRICK,
+            8
+        )
+        var line_start: Vector2 = Vector2(self._rad_in, 0).rotated(segment_width * self._selected)
+        draw_line(Vector2.ZERO + line_start, Vector2.ZERO + self._rad_out/self._rad_in*line_start, Color.FIREBRICK, 8)
+        line_start = Vector2(self._rad_in, 0).rotated(segment_width * (1 + self._selected))
+        draw_line(Vector2.ZERO + line_start, Vector2.ZERO + self._rad_out/self._rad_in*line_start, Color.FIREBRICK, 8)
 
 
-func _on_gui_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
     if (event is InputEventMouseButton):
-        if (
-            event.is_pressed()
-            and not self._menu_open
-        ):
-            self._menu_open = true
-            for child in self._children:
-                child.visible = true
-            self.queue_redraw()
-            self.menu_opened.emit()
-        elif (
-            not event.is_pressed()
-            and self._menu_open
-        ):
-            self._menu_open = false
-            for child in self._children:
-                child.visible = false
-            self.queue_redraw()
-            self.menu_closed.emit(-1)
-    elif (
-        event is InputEventMouseMotion
-        and self._menu_open
-    ):
+        if (not event.is_pressed()):
+            self.queue_free()
+            self.menu_closed.emit(self._selected)
+    elif (event is InputEventMouseMotion):
         var new_select: int = -1
-        if Vector2.ZERO.distance_to(event.position - self._center()) < self._rad_in:
+        if Vector2.ZERO.distance_to(event.position - self.position) < self._rad_in:
             if new_select != self._selected:
                 self._selected = new_select
                 self.queue_redraw()
             return
-        var angle: float = Vector2(100, 0).angle_to(event.position - self._center())
+        var angle: float = Vector2(100, 0).angle_to(event.position - self.position)
         if angle < 0:
             angle += 2*PI
         new_select = floor(angle / (2*PI / len(self._children)))
         if new_select != self._selected:
             self._selected = new_select
             self.queue_redraw()
-    self.queue_redraw()
+    queue_redraw() # TODO: remove?
--- visual/buttons/circle_menu_tester.gd
+++ visual/buttons/circle_menu_tester.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 
 const CMenu = preload("res://visual/buttons/CircleMenu.tscn")
-var cmenu: Control = null
+var cmenu: Node2D = null
+@onready var _button = $TextureButton
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,13 +16,22 @@ func _process(delta: float) -> void:
     pass
 
 
-func _on_trigger_gui_input(event: InputEvent) -> void:
-    if (
-        event is InputEventMouseButton
-        and event.is_pressed()
-    ):
-        $Trigger.queue_free()
-        print("menu clicked?")
-        self.cmenu = CMenu.instantiate()
-        self.cmenu.position = $Trigger.position + $Trigger.size / 2
-        self.add_child(cmenu)
+func _on_texture_button_pressed() -> void:
+    print("menu clicked")
+    self.cmenu = CMenu.instantiate()
+    
+    var s: Sprite2D = Sprite2D.new()
+    s.texture = load("res://visual/assets/directory.svg")
+    self.cmenu.add_child(s)
+    s = Sprite2D.new()
+    s.texture = load("res://visual/assets/directory.svg")
+    self.cmenu.add_child(s)
+    self.cmenu.position = self._button.position + self._button.size / 2
+    s = Sprite2D.new()
+    s.texture = load("res://visual/assets/directory.svg")
+    self.cmenu.add_child(s)
+    
+    self.cmenu.popup(self)
+    self._button.disabled = true
+    self.cmenu.menu_closed.connect(func (_x): self._button.disabled = false)
+    self.cmenu.menu_closed.connect(func (selection): print("Selected ", selection))
