ccb288b97a9a4105f01dd5116b0f10f1a8c0b170
Curtis Barnhart <cbarnhart@westmont.edu>
2025-03-05 09:54:44 -0800

    fixed some static type hinting in integration tests

--- visual/file_nodes/test_base_node.gd
+++ visual/file_nodes/test_base_node.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
+const BaseNode = GVSClassLoader.visual.file_nodes.BaseNode
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    $BaseNode.setup("here is a very long name")
-    $BaseNode.interp_movement(Vector2(600, 600))
-    pass # Replace with function body.
+    ($BaseNode as BaseNode).setup("here is a very long name")
+    ($BaseNode as BaseNode).interp_movement(Vector2(600, 600))
