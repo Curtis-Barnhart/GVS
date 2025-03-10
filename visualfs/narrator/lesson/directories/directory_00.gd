26bdb0aa625cb25d295471681c690cd8e397c79e
Curtis Barnhart <cbarnhart@westmont.edu>
2025-03-10 01:15:53 -0700

    started working on directory lessons

--- visualfs/narrator/lesson/directories/directory_00.gd
+++ visualfs/narrator/lesson/directories/directory_00.gd
@@ -2,6 +2,7 @@ extends "res://visualfs/narrator/lesson/checkpoint.gd"
 
 const UtilString = GVSClassLoader.shared.Strings
 const FileList = GVSClassLoader.visualfs.FileList
+const FileTree = GVSClassLoader.visual.FileTree
 const Path = GVSClassLoader.gvm.filesystem.Path
 const File = GVSClassLoader.visual.file_nodes.BaseNode
 const Menu = GVSClassLoader.visual.buttons.CircleMenu
@@ -10,91 +11,38 @@ const FileReader = GVSClassLoader.visual.FileReader
 const FileWriter = GVSClassLoader.visual.FileWriter
 const FCreateInput = GVSClassLoader.visual.SimpleInput
 
-var _file_list: FileList
-var _current_target: Path
+var _file_tree: FileTree
 
 
 func start() -> void:    
-    self._file_list = self._viewport.node_from_scene("FileList")
-    self._file_list.file_clicked.connect(self.menu_popup)
-    self._next_button.pressed.connect(self.finish)
-    
-    self.make_fake_files(10)
-    self.set_current_target()
-    self._fs_man.removed_file.connect(self.more_files)
+    self._viewport.node_from_scene("FileList").queue_free()
+    for old_file: Path in self._fs_man.read_files_in_dir(Path.ROOT):
+        self._fs_man.remove_file(old_file)
     
     self._text_display.text = UtilString.make_article(
         [
-            "Finding Files",
+            "Finding Files... the Smart Way",
             [
-                "Now we're going to explain some shortcomings",
-                "of just using names to identify files.",
-                "To demonstrate this,",
-                "please locate the file",
-                "%s." % self._current_target.as_string(false),
-                "Delete the file to show that you've located it."
+                "Let's introduce a new way of finding files.",
+                "It might seem more complicated at first,",
+                "but it uses a system of organization that",
+                "allows us to find specific files",
+                "even when we are surrounded by a multitude of information.",
             ],
-        ]
-    )
-
-
-func more_files(deleted: Path) -> void:
-    if self._current_target.as_string() != deleted.as_string():
-        return
-    
-    await GVSGlobals.wait(2)
-    self.make_fake_files(20)
-    self.set_current_target()
-    self._fs_man.removed_file.disconnect(self.more_files)
-    self._fs_man.removed_file.connect(self.even_more_files)
-    self._text_display.text = UtilString.make_article(
-        [
-            "Finding Files",
             [
-                "Not bad... but what happens when add a few more files?",
-                "Find the file '%s'" % self._current_target.as_string(false),
-                "and delete it to show you've found."
+                "I'm going to make about a hundred different files,",
+                "and I'm going to ask you to delete a certain one",
+                "just like in the last section.",
+                "This time, however, we're going to use a system of directions",
+                "that will help you locate it with ease.",
+                "Each next direction will get you closer to the file,",
+                "just like each next direction on a GPS",
+                "takes you closer to your destination when you travel."
             ],
-        ]
-    )
-
-
-func even_more_files(deleted: Path) -> void:
-    if self._current_target.as_string() != deleted.as_string():
-        return
-    
-    await GVSGlobals.wait(2)
-    self._next_button.text = "Give up?"
-    self._next_button.disabled = false
-    self.make_fake_files(70)
-    self.set_current_target()
-    self._fs_man.removed_file.disconnect(self.even_more_files)
-    self._fs_man.removed_file.connect(self.found_everything)
-    
-    self._text_display.text = UtilString.make_article(
-        [
-            "Finding Files",
             [
-                "One last challenge (this one is optional)...",
-                "Find the file '%s'." % self._current_target.as_string(false),
-                "and delet it to show you've found."
-            ],
-        ]
-    )
-
-
-func found_everything(deleted: Path) -> void:
-    if self._current_target.as_string() != deleted.as_string():
-        return
-    
-    self._next_button.text = "Continue"
-    self._fs_man.removed_file.disconnect(self.found_everything)
-    
-    self._text_display.text = UtilString.make_article(
-        [
-            "Finding Files",
-            [
-                "Good job lol."
+                "You should notice that there are other icons",
+                "besides just the file icons now.",
+                "These other icons "
             ],
         ]
     )
@@ -103,7 +51,7 @@ func found_everything(deleted: Path) -> void:
 func menu_popup(file_path: Path) -> void:
     var menu: Menu = Menu.make_new()
     var f0 := Sprite2D.new()
-    var file_vis: File = self._file_list.get_file(file_path)
+    var file_vis: File = self._file_tree.get_file(file_path)
     f0.texture = load("res://visual/assets/file_read.svg")
     menu.add_child(f0)
     f0 = Sprite2D.new()
@@ -159,7 +107,7 @@ func create_file_flow(where: File) -> void:
 
 
 func file_read_popup(path: Path) -> void:
-    var file_vis: File = self._file_list.get_file(path)
+    var file_vis: File = self._file_tree.get_file(path)
     var reader := FileReader.make_new()
     var popup := GPopup.make_into_popup(reader)
     popup.position = file_vis.get_viewport().get_screen_transform() \
@@ -169,7 +117,7 @@ func file_read_popup(path: Path) -> void:
 
 
 func file_write_popup(path: Path) -> void:
-    var file_vis: File = self._file_list.get_file(path)
+    var file_vis: File = self._file_tree.get_file(path)
     var writer := FileWriter.make_new()
     var popup := GPopup.make_into_popup(writer)
     popup.position = file_vis.get_viewport().get_screen_transform() \
@@ -188,7 +136,7 @@ func file_write_popup(path: Path) -> void:
 func finish() -> void:
     self._next_button.text = "Continue"
     self.completed.emit(
-        preload("res://visualfs/narrator/lesson/directories/directory_00.gd").new(
+        preload("res://visualfs/narrator/lesson/completion.gd").new(
             self._fs_man, self._next_button, self._text_display, self._viewport
         )
     )
@@ -217,8 +165,3 @@ func make_fake_files(count: int) -> void:
         self._fs_man.create_file(Path.new([self.generate_fake_filename()]))
         await GVSGlobals.wait(t)
         t = max(0.95*t, 1.0/16)
-
-
-func set_current_target() -> void:
-    var all_files: Array = self._fs_man.read_files_in_dir(Path.ROOT)
-    self._current_target = all_files[randi() % all_files.size()]
