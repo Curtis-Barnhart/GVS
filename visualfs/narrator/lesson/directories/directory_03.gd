c4324d3d2ba6383b7070048a563dc8e1a46dd528
Curtis Barnhart <cbarnhart@westmont.edu>
2025-03-14 09:45:51 -0700

    Added box in directory3 lesson showing the path of last clicked item in a label

--- visualfs/narrator/lesson/directories/directory_03.gd
+++ visualfs/narrator/lesson/directories/directory_03.gd
@@ -13,13 +13,10 @@ var _targets: PackedStringArray = [
     "/"
 ]
 var _target_index: int = 0
+var _path_label := Label.new()
 
 
 func start() -> void:            
-    self._file_tree = self._viewport.node_from_scene("FileTree")
-    self._file_tree.file_clicked.connect(self.file_clicked)
-    self._next_button.pressed.connect(self.finish)
-    
     self._text_display.text = UtilString.make_article(
         [
             "What is a Path?",
@@ -27,8 +24,8 @@ func start() -> void:
                 "This way of identifying files by providing a route",
                 "to them is pretty handy!",
                 "If you had a thousand files to search through,",
-                "instead of looking if each one of them",
-                "had the name of the file you were looking for,",
+                "instead of checking every single one to see if it was",
+                "the one you wanted",
                 "you could just follow the route exactly to the right one.",
             ],
             [
@@ -45,6 +42,18 @@ func start() -> void:
                 "I could just give you the path '/directory0/subdirectory1/file0',",
                 "and you would have all the information you need to find it.",
             ],
+        ]
+    )
+    
+    self._file_tree = self._viewport.node_from_scene("FileTree")
+    self._next_button.disabled = false
+    self._next_button.pressed.connect(self.section2)
+
+
+func section2() -> void:
+    self._text_display.text = UtilString.make_article(
+        [
+            "What is a Path?",
             [
                 "The intermediary places which are not themselves files",
                 "but contain routing information to other files",
@@ -57,15 +66,25 @@ func start() -> void:
             ],
             [
                 "One nice thing about the structure of files and directories",
-                "is that files can only be listed in a single directory,",
-                "which means that there is only ever one valid path to",
-                "that exact file",
-                "(this is technically a lie,",
-                "but explaining how is beyond the scope of these lessons).",
+                "is that files can only be contained in a single directory",
+                "(In our map example, this means that there is only one road",
+                "to get to any given place).",
+                "This means that there is only one valid path to any given file,",
+                "so you can't have two different paths that end up at the same place.",
             ],
+        ]
+    )
+    self._next_button.pressed.disconnect(self.section2)
+    self._next_button.pressed.connect(self.section3)
+
+
+func section3() -> void:
+    self._text_display.text = UtilString.make_article(
+        [
+            "What is a Path?",
             [
                 "To get a little more practice with finding files",
-                "and working with paths in,",
+                "and working with paths,",
                 "let's have you identify a few more files and directories",
                 "based solely on their paths.",
                 "During this section, you won't need to click on each",
@@ -73,7 +92,16 @@ func start() -> void:
                 "or directory to locate it.",
                 "However, if you find it helpful to build the path up piece by piece,",
                 "feel free to do so.",
-                "Click on the file or directory indicated by the paths",
+            ],
+            [
+                "To help you understand paths, I'll add a display at the top",
+                "of the file structure in the left window that will display",
+                "the path of any file or directory you click on.",
+                "Even if the file you clicked on wasn't correct,",
+                "you'll be able to see what the path to that file is,",
+                "which will help you understand how that path is different",
+                "from the one you are looking for.",
+                "Click on the file or directory indicated by the following paths",
                 "to complete this section:"
             ],
             [
@@ -82,11 +110,26 @@ func start() -> void:
         ]
     )
     
+    self._file_tree.file_clicked.connect(self.file_clicked)
+
+    self._path_label.add_theme_font_override("font", load("res://shared/JetBrainsMonoNerdFontMono-Regular.ttf") as Font)
+    self._path_label.add_theme_font_size_override("font_size", 48)
+    self._path_label.add_theme_stylebox_override("normal", load("res://shared/TextBox.tres") as StyleBox)
+    self._path_label.size.x = 1000
+    self._path_label.position = Vector2(-500, -160)
+    self._path_label.text = "Hello world!"
+    self._path_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+    self._viewport.add_to_scene(self._path_label)
+
     self._text_display.scroll_following = true
+    self._next_button.disabled = true
+    self._next_button.pressed.disconnect(self.section3)
+    self._next_button.pressed.connect(self.finish)
 
 
 func file_clicked(file_path: Path) -> void:
     self._file_tree.highlight_path(Path.ROOT, file_path)
+    self._path_label.text = file_path.as_string()
     if (
         self._target_index < self._targets.size()
         and file_path.as_string() == self._targets[self._target_index]
