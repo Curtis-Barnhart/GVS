217122e8b2d2fb6b7588708d35fe8fd65581baed
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-15 12:31:42 -0700

    formatting

--- visual/file_tree.gd
+++ visual/file_tree.gd
@@ -33,13 +33,13 @@ class HighlightData extends RefCounted:
     class Record:
         var path: String
         var tcolor_id: int
-        
+
     var _data: Array[Record] = []
     var _id: int
-    
+
     func _init(id: int) -> void:
         self._id = id
-    
+
     func add_data(path: String, tcolor_id: int) -> void:
         var r := Record.new()
         r.path = path
@@ -70,13 +70,13 @@ class HighlightServer extends RefCounted:
             push_warning("HighlightStack contains %d elements, more than I meant it to hold." % self._highlights.size())
 
         return id
-    
+
     func _get_nodes_to_color(
         origin: Path,
         path: Path
     ) -> Dictionary[String, Object]:
         var nodes: Dictionary[String, Object] = {}
-        
+
         var dest: Path = origin.compose(path)
         while origin.as_string() != dest.as_string():
             var next_hop: String = path.head()
@@ -90,7 +90,7 @@ class HighlightServer extends RefCounted:
             path = path.tail()
 
         return nodes
-    
+
     func push_color_to_tree_nodes(
         color: Color,
         origin: Path,
@@ -98,13 +98,13 @@ class HighlightServer extends RefCounted:
     ) -> int:
         var id: int = self._get_next_id()
         var hl_data := HighlightData.new(id)
-        
+
         for file_str: String in self._get_nodes_to_color(origin, path).keys():
             var node: TNode = self._nodes[file_str]
             hl_data.add_data(file_str, node.color_stack.push_solid_color(color))
             node.z_index = 1
             node.queue_redraw()  # TODO: is this necessary?
-        
+
         self._highlights.push_back(hl_data)
         return id
 
@@ -113,7 +113,7 @@ class HighlightServer extends RefCounted:
         duration: float,
         origin: Path,
         path: Path
-    ) -> void:        
+    ) -> void:
         for file_str: String in self._get_nodes_to_color(origin, path):
             var node: TNode = self._nodes[file_str]
             node.color_stack.push_flash_color(color, duration)
@@ -126,7 +126,7 @@ class HighlightServer extends RefCounted:
             # that such a system would actually be used.
             # hl_data._data.push_back([file_str, node.color_stack.push_flash_color(color, duration)])
             node.z_index = 1
-        
+
         # TODO: This look dumb? That's cause it is.
         # Unfortunately SceneTree.create_timer is inaccurate to like... tenths of a second??
         # That means that we gotta just kinda spray and pray timers and hope one hits it.
@@ -136,7 +136,7 @@ class HighlightServer extends RefCounted:
         GVSGlobals.get_tree() \
                   .create_timer(duration + 0.1) \
                   .timeout.connect(func () -> void: self.clear_z_index(origin, path))
-    
+
     func clear_z_index(
         origin: Path,
         path: Path
@@ -177,7 +177,7 @@ func highlight_path(origin: Path, path: Path) -> void:
             any_node._path_glow = false
             any_node.z_index = 0
             any_node.queue_redraw()
-        
+
     # highlight new path
     self._hl_origin = origin
     self._hl_path = path
@@ -243,8 +243,8 @@ func collapse_dir(p: Path) -> void:
     )
     dir.change_icon(dir_text)
     dir.collapse()
-    
-    
+
+
 ## Tells a directory is to visually uncollapse.
 ## [code]p[/code] must be in simplest form.
 ## Asserts that the directory actually exists.[br][br]
@@ -264,14 +264,14 @@ class FaderIn extends Node:
     var _host: CanvasItem
     var _t0: float
     var _duration: float
-    
+
     static func attach(host: CanvasItem, duration: float) -> void:
         var fader := FaderIn.new()
         host.add_child(fader)
         fader._host = host
         fader._t0 = Time.get_unix_time_from_system()
         fader._duration = duration
-    
+
     func _process(_delta: float) -> void:
         var diff: float = (Time.get_unix_time_from_system() - self._t0) / self._duration
         self._host.modulate.a = min(MathUtils.unary_log_interp(diff), 1)
@@ -293,7 +293,7 @@ func create_node(p: Path, texture: Texture2D) -> void:
         "Attempted TNode creation with nonexistent parent."
     )
     var child := TNode.make_new()
-    
+
     child.modulate.a = 0
     FaderIn.attach(child, 2)
     child.z_index = -1
@@ -301,7 +301,7 @@ func create_node(p: Path, texture: Texture2D) -> void:
         func () -> void:
             child.z_index = 0
     )
-    
+
     parent.add_subnode(child, p.last())
     self._all_nodes[p.as_string()] = child
     child.change_icon(texture)
@@ -350,7 +350,7 @@ func _ready() -> void:
     self._all_nodes["/"].z_index = 0
     self.change_cwd(Path.ROOT, Path.ROOT)
     self._all_nodes["/"]._icon.pressed.connect(func () -> void: self.file_clicked.emit(Path.ROOT))
-    
+
     self.hl_server = HighlightServer.new(self._all_nodes)
 
 
