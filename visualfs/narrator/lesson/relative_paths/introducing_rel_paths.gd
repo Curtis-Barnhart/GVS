7eed23ab4fb7452e795e812e9d8ecc5387e7873c
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-14 21:06:18 -0700

    got the main mechanics of introducing relative paths lesson

--- visualfs/narrator/lesson/relative_paths/introducing_rel_paths.gd
+++ visualfs/narrator/lesson/relative_paths/introducing_rel_paths.gd
@@ -7,10 +7,6 @@ const FileTree = GVSClassLoader.visual.FileTree
 const TNode = GVSClassLoader.visual.file_nodes.TreeNode
 
 var _file_tree: FileTree
-var _target_paths: Array[Path] = [
-    Path.ROOT
-]
-var _target_index: int = 0
 var _active_hl: int = -1
 var _path_label: RichTextLabel
 
@@ -87,8 +83,16 @@ func start(needs_context: bool) -> void:
     self._viewport.move_cam_to(Vector2(0, TNode.HEIGHT * 1.5))
     await GVSGlobals.wait(1)
 
-    self._next_button.pressed.connect(self.show_relative_path)
+
+    self._next_button.pressed.connect(self._set_cwd)
     self._next_button.disabled = false
+
+
+func _set_cwd() -> void:
+    self._file_tree.cwd_text = preload("res://visual/assets/cwd_open.svg")
+    self._file_tree.change_cwd(Path.new(["school", "homework"]), Path.ROOT)
+    self._next_button.pressed.disconnect(self._set_cwd)
+    self._next_button.pressed.connect(self.show_relative_path)
 
 
 func show_relative_path() -> void:
@@ -147,16 +151,18 @@ func show_relative_path_parents() -> void:
     )
     self._next_button.disabled = false
     self._next_button.pressed.disconnect(self.show_relative_path_parents)
-    self._next_button.pressed.connect(self.finish)
+    self._next_button.pressed.connect(self.show_relative_path_all)
     self._file_tree.file_clicked.disconnect(self._display_children)
-    self._file_tree.file_clicked.connect(self.display_parents)
+    self._file_tree.file_clicked.connect(self._display_parents)
 
     if self._active_hl >= 0:
         self._file_tree.hl_server.pop_id(self._active_hl)
         self._active_hl = -1
 
+    self._write_path_label(" ")
 
-func display_parents(p: Path) -> void:
+
+func _display_parents(p: Path) -> void:
     var h_path := Path.new(["school", "homework"])
     if self._active_hl >= 0:
         self._file_tree.hl_server.pop_id(self._active_hl)
@@ -167,9 +173,57 @@ func display_parents(p: Path) -> void:
             Color.GREEN, h_path, self._fs_man.relative_to(p, h_path)
         )
         self._write_path_label(self._fs_man.relative_to(p, h_path).as_string(false))
-    elif p.as_string() != h_path.as_string():
+    else:
         self._file_tree.hl_server.push_flash_to_tree_nodes(Color.RED, 2.0, h_path, self._fs_man.relative_to(p, h_path))
         self._write_path_label(" ")
+
+
+func show_relative_path_all() -> void:
+    if self._active_hl >= 0:
+        self._file_tree.hl_server.pop_id(self._active_hl)
+        self._active_hl = -1
+
+    self._text_display.text = UtilString.make_article(
+        [
+            "Relative Paths",
+            [
+                "look you can even go back down after you go up"
+            ]
+        ]
+    )
+
+    self._next_button.disabled = false
+    self._next_button.pressed.disconnect(self.show_relative_path_all)
+    self._next_button.pressed.connect(self.finish)
+
+    self._fs_man.create_dir(Path.new(["pictures"]))
+    await GVSGlobals.wait(0.5)
+    self._fs_man.create_file(Path.new(["pictures", "something"]))
+    await GVSGlobals.wait(0.5)
+
+    self._file_tree.file_clicked.disconnect(self._display_parents)
+    self._file_tree.file_clicked.connect(self._display_all)
+
+    if self._active_hl >= 0:
+        self._file_tree.hl_server.pop_id(self._active_hl)
+        self._active_hl = -1
+
+    self._write_path_label(" ")
+
+
+func _display_all(p: Path) -> void:
+    var h_path := Path.new(["school", "homework"])
+    if self._active_hl >= 0:
+        self._file_tree.hl_server.pop_id(self._active_hl)
+        self._active_hl = -1
+
+    if p.as_string() == h_path.as_string():
+        self._write_path_label(".")
+    else:
+        self._active_hl = self._file_tree.hl_server.push_color_to_tree_nodes(
+            Color.GREEN, h_path, self._fs_man.relative_to(p, h_path)
+        )
+        self._write_path_label(self._fs_man.relative_to(p, h_path).as_string(false))
 
 
 func finish() -> void:
@@ -181,7 +235,7 @@ func finish() -> void:
     self._inst.render()
 
     self.completed.emit(
-        preload("res://visualfs/narrator/lesson/completion.gd").new()
+        preload("res://visualfs/narrator/lesson/relative_paths/reading_rel_paths.gd").new()
     )
     assert(
         self.get_reference_count() == 1,
